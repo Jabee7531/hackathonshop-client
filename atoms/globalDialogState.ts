f9bb42b48680b3b15226223eae67cfd1cae6a0ba
JaBee <JaBee7531@gmail.com>
2022-08-09 01:04:30 +0900

    fix recoil Duplicate atom key bug

--- atoms/globalDialogState.ts
+++ atoms/globalDialogState.ts
@@ -1,7 +1,8 @@
 import { atom } from "recoil";
+import { v1 } from "uuid";
 
 export const globalDialogState = atom({
-  key: "globalDialogState",
+  key: `globalDialogState/${v1()}`,
   default: {
     title: "",
     message: "",
